761315a36d928dc8e5ca2c4f57bb187b4314adad
Thenlie <leithen113@gmail.com>
2024-09-05 21:27:26 -0600

    improve menu item component

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -4,22 +4,29 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 const Menu = () => {
   return (
     <SafeAreaView>
-      <Text>Menu</Text>
+      <View style={styles.titleContainer}>
+        <Text style={{ fontSize: 24 }}>Menu</Text>
+      </View>
       <View style={styles.container}>
-        <MenuItem title="Add a Guest" />
-        <MenuItem title="View Lists" />
-        <MenuItem title="Settings" />
+        <MenuItem title="Add a Guest" startIcon="people" />
+        <MenuItem title="View Lists" startIcon="list" />
+        <MenuItem title="Settings" startIcon="cog" isLastItem={true} />
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   container: {
     margin: 4,
-    borderWidth: 1,
-    borderColor: 'grey',
+    borderWidth: 0.5,
+    borderColor: 'lightgrey',
     borderRadius: 4,
+    backgroundColor: 'white',
   },
 });
 
--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -3,30 +3,68 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 interface MenuItemProps {
   title: string;
+  startIcon?: keyof typeof Ionicons.glyphMap;
+  isLastItem?: boolean;
 }
 
-const MenuItem = ({ title }: MenuItemProps) => {
+/**
+ * A single item within the menu
+ */
+const MenuItem = ({ title, startIcon, isLastItem = false }: MenuItemProps) => {
+  const pressHandler = () => {
+    console.log('Pressed!');
+  };
+
   return (
-    <Pressable style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text>{title}</Text>
-        <Ionicons name="chevron-forward" size={16} />
-      </View>
+    <Pressable
+      style={({ pressed }) => [
+        styles.container,
+        { backgroundColor: pressed ? 'lightgrey' : 'white' },
+      ]}
+      onPress={pressHandler}>
+      {({ pressed }) => (
+        <>
+          {startIcon && (
+            <View style={styles.iconContainer}>
+              <Ionicons name={startIcon} size={24} />
+            </View>
+          )}
+          <View style={[styles.borderWrapper, isLastItem && { borderBottomWidth: 0 }]}>
+            <Text style={styles.text}>{title}</Text>
+            <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward" size={18} color="grey" />
+            </View>
+          </View>
+        </>
+      )}
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 8,
-    borderBottomColor: 'lightgrey',
-    borderBottomWidth: 1,
-    textAlignVertical: 'center',
-  },
-  innerContainer: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
+    width: '100%',
+  },
+  iconContainer: {
+    padding: 8,
+  },
+  borderWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '90%',
+    borderBottomColor: 'lightgrey',
+    borderBottomWidth: 0.5,
+    height: 40,
+  },
+  text: {
+    padding: 4,
+    fontWeight: '500',
   },
 });
 
